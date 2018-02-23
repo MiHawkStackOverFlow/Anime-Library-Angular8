a2fcda36151609856107e176dad0f3675000a5fe
abhivsharma8 <abhivsharma8@gmail.com>
2018-02-23 13:21:04 -0800

    refactor: initial changes to test file and minor other changes.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,17 +16,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
   
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as heading "Abhishek's Library"`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual("Abhishek's Library");
   }));
   
-  it('should render title in a h1 tag', async(() => {
+  it('should render heading in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain("Abhishek's Library");
   }));
-  
+
 });
