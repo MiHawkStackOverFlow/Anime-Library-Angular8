0adecf88bba067cfcb5c139bbf159b66e6a462ac
MiHawkStackOverFlow <abhivsharma8@gmail.com>
2018-06-30 02:54:04 -0700

    app migrated to angular6 using https://update.angular.io.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,18 +15,18 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  
-  it(`should have as heading "Abhishek's Library"`, async(() => {
+
+  it(`should have as heading 'Abhishek's Library'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual("Abhishek's Library");
+    expect(app.title).toEqual('Abhishek"s Library');
   }));
-  
+
   it('should render heading in a href of h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').querySelector('a').title).toContain("Abhishek's Library");
+    expect(compiled.querySelector('h1').querySelector('a').title).toContain('Abhishek"s Library');
   }));
 
 });
