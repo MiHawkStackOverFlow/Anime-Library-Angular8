bf3e9e4938fbf598d71c65eb7c2a719f93731506
MiHawkStackOverFlow <abhivsharma8@gmail.com>
2018-07-03 21:05:00 -0700

    css and html changes for header.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   title = 'Abhishek"s Anime Library';
-  photo = 'http://otakukart.com/animeblog/wp-content/uploads/2016/05/Awesome-ANime-Villains.png';
+  photo = 'https://www.wallpaperup.com/uploads/wallpapers/2014/03/24/308257/8e834bc087354f282ca06f4d6c92b37d-700.jpg';
 }
