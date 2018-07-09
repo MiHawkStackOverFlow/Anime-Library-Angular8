7c3a142f91690fae0304b48b3382e3a583e96325
Abhishek <absharma@lantronix.com>
2018-07-09 18:03:20 +0530

    corrected bootstrap imports and then css changes.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SpinnerComponent } from './spinner/spinner.component';
 import { SpinnerService } from './spinner/spinner.service';
 
 @Component({
@@ -8,12 +7,31 @@ import { SpinnerService } from './spinner/spinner.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Abhishek"s Anime Library';
+  title = `Abhishek's Anime Library`;
   photo = 'https://www.wallpaperup.com/uploads/wallpapers/2014/03/24/308257/8e834bc087354f282ca06f4d6c92b37d-700.jpg';
+
+  sliderImages = [ 
+                    { img: 'https://www.animelab.com/assets/images/home/covers/attack-on-titan-s2.png',
+                      name: 'Attack On Titan',
+                      description: 'Attack on Titan is a Japanese manga series written and illustrated by Hajime Isayama'
+                    },
+                    { img: 'https://www.animelab.com/assets/images/home/covers/dragonballsuper.png',
+                      name: 'Dragon Ball Super',
+                      description: 'Dragon Ball Super is a Japanese anime television series produced by Toei Animation'
+                    },
+                    { img: 'https://www.animelab.com/assets/images/home/covers/one-punch-man.png',
+                      name: 'One Punch Man',
+                      description: 'One-Punch Man is an ongoing Japanese superhero webcomic created by ONE'
+                    },
+                    { img: 'https://www.animelab.com/assets/images/home/covers/naruto-shippuden-purple.png',
+                      name: 'Naruto Shippuden',
+                      description: `Naruto: Shippuden is an anime series adapted from Part II of Masashi Kishimoto's manga series, with exactly 500 episodes.`
+                    }
+                 ];
 
   constructor(public spinner: SpinnerService){} 
 
-  ngOnInit(){
+  ngOnInit() {
     this.spinner.start();
   }
 
