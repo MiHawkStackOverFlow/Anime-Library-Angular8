8d7f57ff8921f678bb30c99876b449d309a2801d
Abhishek <absharma@lantronix.com>
2018-07-20 09:26:37 +0530

    feat: using @Input to get page title.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  title: string = `Abhishek's Anime Library`;
+  @Input() title: string;
   today: number = Date.now();
   
   constructor() { }
