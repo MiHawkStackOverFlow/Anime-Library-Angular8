c107f1c5634a3a29ee1a9e86f06a1c6fa333c1c5
Abhishek <absharma@lantronix.com>
2018-07-20 23:25:44 +0530

    refactor: deleted tabular module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SharedModule } from'./shared/shared.module';
 import { AppRoutingModule } from './/app-routing.module';
-import { TabularModule } from './tabular/tabular.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     SharedModule,
-    AppRoutingModule,
-    TabularModule
+    AppRoutingModule
   ],
   declarations: [
     AppComponent
