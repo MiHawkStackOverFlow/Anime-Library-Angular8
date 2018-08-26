8a86c22a9883962c5db560198c488a6fef5b19e8
Abhishek <absharma@lantronix.com>
2018-08-26 12:05:21 +0530

    refactor: refactored landing page and added new mock data.

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, animate, transition } from '@angular/animations';
-declare var $: any;
 import { timer, Observable, Subscription } from 'rxjs';
+
+import { Carousel } from '../shared/interfaces/carousel';
+import { Villain } from '../shared/interfaces/villain';
+
+import { sliderImages } from '../shared/mock-data/mock-carousel';
+import { villains } from '../shared/mock-data/mock-villains';
 
 @Component({
   selector: 'app-landing-page',
@@ -21,51 +26,20 @@ import { timer, Observable, Subscription } from 'rxjs';
   ]
 })
 export class LandingPageComponent implements OnInit {
-  id: any;
-  flip1: string = 'inactive';
-  flip2: string = 'inactive';
-  flip3: string = 'inactive';
-  flip4: string = 'inactive';
-  
-  subscribe: Subscription;
-  villainsSectionHeading: string = 'Popular Anime Villains Across Popular Anime Series';  
-  sliderImages: Array<Object> = [ 
-    { img: 'https://www.animelab.com/assets/images/home/covers/attack-on-titan-s2.png',
-      name: 'Attack On Titan',
-      description: 'Attack on Titan is a Japanese manga series written and illustrated by Hajime Isayama'
-    },
-    { img: 'https://www.animelab.com/assets/images/home/covers/dragonballsuper.png',
-      name: 'Dragon Ball Super',
-      description: 'Dragon Ball Super is a Japanese anime television series produced by Toei Animation'
-    },
-    { img: 'https://www.animelab.com/assets/images/home/covers/one-punch-man.png',
-      name: 'One Punch Man',
-      description: 'One-Punch Man is an ongoing Japanese superhero webcomic created by ONE'
-    },
-    { img: 'https://www.animelab.com/assets/images/home/covers/naruto-shippuden-purple.png',
-      name: 'Naruto Shippuden',
-      description: `Naruto: Shippuden is an anime series adapted from Part II of Masashi Kishimoto's manga series, with exactly 500 episodes.`
-    }
-  ];
+  flip: string = 'inactive';
+  villainsSectionHeading: string = 'Popular Anime Villains Across Popular Anime Series';
+  animeVillains: Array<Villain> = villains; 
 
-  constructor(private ngZone: NgZone) { }
+  // data for carousel component
+  height: number = 500; 
+  sliderImages: Array<Carousel> = sliderImages;
 
-  ngOnInit() {
-      let source: Observable<number> = timer(3000, 4000);
-      this.subscribe = source.subscribe(() => {
-        ($("#myCarousel") as any).carousel("next");
-      });    
-  }
+  constructor() { }
 
-  toggleFlip(flipNum: number) {
-    let flipper = 'flip' + flipNum;
-    this[flipper] = (this[flipper] == 'inactive') ? 'active' : 'inactive';
-  }
+  ngOnInit() { }
 
-  ngOnDestroy() {
-    if(this.subscribe) {
-      this.subscribe.unsubscribe();
-    }
+  toggleFlip(villain: Villain) {
+   villain['flip'] = (villain['flip'] == 'active') ? 'inactive' : 'active';
   }
 
 }
---
+++ src/app/shared/mock-data/mock-villains.ts
@@ -0,0 +1,30 @@
+import { Villain } from '../interfaces/villain';
+
+export const villains: Array<Villain> = [ 
+    { image: 'https://img00.deviantart.net/4566/i/2018/023/c/e/vegeta_by_wizyakuza-dc0zqa9.jpg',
+      alt: 'Vegeta Dragon Ball',
+      description: `Vegeta is the prince of the fallen Saiyan race. Regal, egotistical, and full of pride,
+      Vegeta was once a ruthless, cold-blooded warrior and outright killer, but later abandons his role, 
+      fighting alongside the universe's most powerful warrior, specifically with the 
+      mission to defeat and surpass Goku in power. His character evolves from villain, to anti-hero, to a hero through the course of the series.`
+    },
+    { image: 'http://i66.tinypic.com/2ql5x14.jpg',
+      alt: 'Code Geass Lelouch',
+      description: `Lelouch vi Britannia was the protagonist of Code Geass.He was the Eleventh Prince of the 
+      Holy Britannian Empire and the son of the 98th Emperor of Britannia, Charles vi Britannia. He was also the leader of 
+      the Black Knights and the real identity of Zero.In battle, Lelouch is very cold and tactical. He is willing to 
+      sacrifice civilians and soldiers alike, if that is what it takes to achieve the objective.`
+    },
+    { image: 'https://takao21203.files.wordpress.com/2013/08/tumblr_mh7q75rnog1rkq9cno1_500.png',
+      alt: 'Kai Hiwatari Beyblade',
+      description: `Kai Hiwatari is one of the deuteragonists from the Original Series, consisting of Beyblade: G-Revolution.
+      Kai is a proud and a very confident blader who is willing to sacrifice everything to get what he wants. He would much prefer earning things through hard work. However, when he returns to his old self, he does so with an even stronger 
+      determination to win. He is a quiet and mysterious person.`
+    },
+    { image: 'https://pre00.deviantart.net/3340/th/pre/f/2017/049/f/6/f68fdfc3225f63fb62eaf1b64a6acd6d-daqdm9c.jpg',
+      alt: 'Zoro One Piece',
+      description: `Roronoa Zoro, also known as "Pirate Hunter" Zoro,is the swordsman of the Straw Hat Pirates and a former bounty hunter.
+      He is considered the largest threat and most dangerous member in it after Luffy. His fame as a master swordsman and his great strength, 
+      along with the actions of his captain, sometimes led others to believe he was the true captain of the crew before he obtained his first bounty.`
+    }
+];
