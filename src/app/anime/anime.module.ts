bd6740580837159e2b21b66180a64827011817a7
MiHawkStackOverFlow <abhivsharma8@gmail.com>
2018-12-25 12:55:10 -0800

    feat: init setup with anime in memory service creation.

--- src/app/anime/anime.module.ts
+++ src/app/anime/anime.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from '../app-routing.module';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AnimeService } from './services/anime.service';
 
 import { AnimeListComponent } from './components/anime-list/anime-list.component';
 import { PopularAnimeComponent } from './components/popular-anime/popular-anime.component';
@@ -9,7 +11,14 @@ import { TableComponent } from './components/table/table.component';
 import { AnimePicListComponent } from './components/anime-pic-list/anime-pic-list.component';
 
 @NgModule({
-  imports: [ CommonModule, AppRoutingModule ],
+  imports: [ CommonModule, AppRoutingModule,
+  // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      AnimeService, { dataEncapsulation: false }
+    )
+  ],
   exports: [ AnimeListComponent, PopularAnimeComponent, TableComponent ],
   declarations: [ AnimeListComponent, PopularAnimeComponent, TableComponent, AnimePicListComponent ]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
-// angular libraries 
-// Angular loads as a collection of JavaScript modules. You can think of them as library modules. Each Angular library name begins with the @angular prefix.
+// angular libraries
+/* Angular loads as a collection of JavaScript modules.
+   You can think of them as library modules. Each Angular library name begins with the @angular prefix.*/
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/* The HttpClient in @angular/common/http offers a simplified client HTTP API for Angular applications
+   that rests on the XMLHttpRequest interface exposed by browsers.
+   Additional benefits of HttpClient include testability features, typed request and response objects,
+   request and response interception, Observable apis, and streamlined error handling. */
 import { HttpClientModule } from '@angular/common/http';
 import { NgRedux, NgReduxModule } from '@angular-redux/store';
 
 // created modules
 import { AnimeModule } from './anime/anime.module';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from'./shared/shared.module';
+import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 
 // no need to import spinner service because of providedIn flag
@@ -20,7 +25,7 @@ import { AppComponent } from './app.component';
 // redux store
 import { AnimeAppState, rootReducer, INITIAL_STATE } from './core/redux/store';
 
-//iziToast
+// iziToast
 import { Ng2IziToastModule } from 'ng2-izitoast';
 
 @NgModule({
@@ -34,7 +39,7 @@ import { Ng2IziToastModule } from 'ng2-izitoast';
     // app modules
     AnimeModule,
     AppRoutingModule,
-    CoreModule,   
+    CoreModule,
     SharedModule
   ],
   declarations: [
@@ -43,7 +48,7 @@ import { Ng2IziToastModule } from 'ng2-izitoast';
   providers: [ ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { 
+export class AppModule {
   constructor(ngRedux: NgRedux<AnimeAppState>) {
      ngRedux.configureStore(rootReducer, INITIAL_STATE);
   }
