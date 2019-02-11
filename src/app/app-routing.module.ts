7baebbb53326f871133a7499868e54fe7e4eab4d
MiHawkStackOverFlow <abhivsharma8@gmail.com>
2019-02-10 21:26:09 -0800

    refactor: removed lazy loading as modules eagerly loaded.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 
 // components
 import { LandingPageComponent } from './core/components/landing-page/landing-page.component';
+import { AnimeListComponent } from './anime/components/anime-list/anime-list.component';
+import { PopularAnimeComponent } from './anime/components/popular-anime/popular-anime.component';
+import { AnimeDetailComponent } from './anime/components/anime-detail/anime-detail.component';
+import { UserListComponent } from './user/components/user-list/user-list.component';
+import { ContactComponent } from './user/components/contact/contact.component';
+import { OrderComponent } from './user/components/order/order.component';
 
 export const routes: Routes = [
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: LandingPageComponent },
-      { path: 'anime-list', loadChildren: './anime/anime.module#AnimeModule' },
-      { path: 'detail/:id', loadChildren: './anime/anime.module#AnimeModule' },
-      { path: 'popular-anime', loadChildren: './anime/anime.module#AnimeModule' },
-      // { path: 'user-list', loadChildren: './user/user.module#UserModule' },
-      // { path: 'contact-us', loadChildren: './user/user.module#UserModule' },
-      // { path: 'order', loadChildren: './user/user.module#UserModule' }
+      { path: 'anime-list', component: AnimeListComponent },
+      { path: 'popular-anime', component: PopularAnimeComponent },
+      { path: 'detail/:id', component: AnimeDetailComponent },
+      { path: 'user-list', component: UserListComponent },
+      { path: 'contact-us',  component: ContactComponent },
+      { path: 'order',  component: OrderComponent }
 ];
 
 @NgModule({
